113d0f5bbbeb89842ede55871dabaa28e9549238
yun-0609 <GeorgeRidlondu@outlook.com>
2026-08-05 09:30:12 +0900

    test: cover unique

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,3 +2,5 @@ const u = require('../src/utils');
 
 test('reverse is defined', () => { expect(u.reverse).toBeDefined(); });
 
+test('unique is defined', () => { expect(u.unique).toBeDefined(); });
+
