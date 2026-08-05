0f3ef61036c2a6a357537b27bec219de29e7192c
yun-0609 <GeorgeRidlondu@outlook.com>
2026-08-05 09:30:23 +0900

    test: cover clamp

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,3 +4,5 @@ test('reverse is defined', () => { expect(u.reverse).toBeDefined(); });
 
 test('unique is defined', () => { expect(u.unique).toBeDefined(); });
 
+test('clamp is defined', () => { expect(u.clamp).toBeDefined(); });
+
